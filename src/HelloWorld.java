4107e859f697cf87bc970a6ee8c2497b914aea9a
Choudhry Nisar <asifnisar@gmail.com>
2017-04-05 10:15:21 +0100

    mesage chnaged

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
   public static void main (String [] args)
    {
-	System.out.println("hello world, this is my new messagefor auto build docker image");
+	System.out.println("hello world, this is my new messagefor auto build docker image, this file is chnaged in git");
    }
 }
